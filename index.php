d7730bb0f8108ac1b19688b89db9c546571bf5a6
alee2 <alee2@bcchr.ca>
2019-05-01 13:51:35 -0700

    Version fixes.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php 
+<?php
+require_once APP_PATH_DOCROOT . "ProjectGeneral/header.php"; 
 $surveyDashboard = new BCCHR\SurveyDashboard\SurveyDashboard();
 $surveyDashboard->getDashboard();
+require_once APP_PATH_DOCROOT . "ProjectGeneral/footer.php";
