aa49d821daef442ceb2cdbe128b5565a40f41302
BCCHR - REDCap <49417667+BCCHR-IT@users.noreply.github.com>
2025-07-28 11:18:19 -0700

    Delete survey_dashboard_v1.3 directory

--- survey_dashboard_v1.3/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once APP_PATH_DOCROOT . "ProjectGeneral/header.php"; 
-$surveyDashboard = new BCCHR\SurveyDashboard\SurveyDashboard();
-$surveyDashboard->getDashboard();
-require_once APP_PATH_DOCROOT . "ProjectGeneral/footer.php";
